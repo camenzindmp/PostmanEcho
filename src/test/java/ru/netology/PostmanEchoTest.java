d4e53950b0672e283d1a93af29ce79a540d11f8b
camenzindmp <camenzindmp@gmail.com>
2020-09-10 21:41:23 +1000

    meh...

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -1,23 +1,27 @@
 package ru.netology;
 
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 class PostmanEchoTest {
     @Test
-    void test1() {
+    void ShouldGiveCorrectResponse() {
         // Given - When - Then
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("text/plain; charset=UTF-8")
+                .body("This is expected to be sent back as part of response body.") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */);
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
     }
 }
